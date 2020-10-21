ee9232529f14864550abe8aae08621724021b654
surajkamtam <30399490+surajkamtam@users.noreply.github.com>
2020-10-21 17:55:05 +0100

    With Age details

--- src/pages/admin/WeeklyCourses.js
+++ src/pages/admin/WeeklyCourses.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Container, Typography } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  Typography,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import axios from "axios";
+import auth from "../../lib/auth";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -21,31 +29,99 @@ const useStyles = makeStyles((theme) => ({
     height: "55%",
     justifyContent: "space-around",
     margin: "20px 0",
+    textDecoration: "inherit",
+    color: "inherit",
   },
 }));
 
 export default function ContainedButtons() {
   const classes = useStyles();
-  const ageGroups = ['4-6', '7-9', '10-12', '13-15', '16-18', 'Adults']
+  //const ageGroups = ["4-6", "7-9", "10-12", "13-15", "16-18", "Adults"];
+  const [state, setState] = React.useState();
+  //const [ageValues, setAgeValues] = React.useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`/users/${auth.getUserId()}`)
+      .then((res) => {
+        console.log(res.data);
+        setState(res.data[0].ageDetails);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
   return (
     <Container className={classes.container}>
       <Typography variant="h4">SELECT THE AGE GROUP</Typography>
-      {
-        ageGroups.map((el, i) => {
-          const age = i !== ageGroups.length - 1 ? `${el} years` : el
+      {/* {!state && (
+        <Button className={classes.form} variant="outlined" color="primary">
+          <Link to="/companyDashboard/addAgeGroup">
+            <Typography variant="h6">Add Age Details</Typography>
+          </Link>
+        </Button>
+      )} */}
+
+      {!state ? (
+        <Card className={classes.form} variant="outlined" color="primary">
+          <Link to="/companyDashboard/addAgeGroup">
+            <CardContent>
+              <Typography variant="h6">Add Age Groups</Typography>
+            </CardContent>
+          </Link>
+        </Card>
+      ) : (
+        state.map((data, i) => {
+          const years =
+            (data.startAge || data.endAge) === `Adults` ? "" : `years old`;
+          let age;
+          if (!data.startAge || data.startAge === data.endAge) {
+            age = `${data.endAge} ${years}`;
+          } else if (!data.endAge) {
+            age = `${data.startAge} ${years}`;
+          } else age = `${data.startAge} - ${data.endAge} ${years}`;
+          //console.log(data.ageDetails.length - 1);
+
           return (
-            <Link to={{
-              pathname: "/companyDashboard/weeklyCourseDetails",
-              state: age
-            }}>
-              <Button className={classes.form} variant="contained" color="primary">
-                {age}
+            <div key={i}>
+              <Button
+                className={classes.form}
+                variant="outlined"
+                color="primary"
+              >
+                <Link
+                  to={{
+                    pathname: "/companyDashboard/weeklyCourseDetails",
+                    state: age,
+                  }}
+                >
+                  <Typography variant="h6">{age}</Typography>
+                </Link>
               </Button>
-            </Link>
-          )
+            </div>
+          );
         })
-      }
+      )}
+      {/* {ageGroups.map((el, i) => {
+        const age = i !== ageGroups.length - 1 ? `${el} years` : el;
+        return (
+          <Link
+            to={{
+              pathname: "/companyDashboard/weeklyCourseDetails",
+              state: age,
+            }}
+          >
+            <Button
+              className={classes.form}
+              variant="contained"
+              color="primary"
+            >
+              {age}
+            </Button>
+          </Link>
+        );
+      })} */}
       <Button className={classes.button} variant="contained" color="primary">
         DONE
       </Button>
@@ -54,6 +130,6 @@ export default function ContainedButtons() {
           Back
         </Button>
       </Link>
-    </Container >
+    </Container>
   );
 }
