a104a6b378925d166bb3fa2970e52ed0ca924a8c
surajkamtam <30399490+surajkamtam@users.noreply.github.com>
2020-10-21 17:55:05 +0100

    With Age details

--- src/pages/admin/AddAgeGroup.js
+++ src/pages/admin/AddAgeGroup.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { Button, Container, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import {
-  makeStyles,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  Select,
-  MenuItem,
-} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import AgeComponent from "./AgeComponent";
+import axios from "axios";
+import auth from "../../lib/auth";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -37,58 +32,84 @@ export default function AddAgeGroup() {
   const classes = useStyles();
   const [rows, setRows] = React.useState([1]);
 
-  const [ageDetails, setAgeDetails] = React.useState({
-    ages: "",
-  });
+  const [ageDetails, setAgeDetails] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(ageDetails);
+
+    axios
+      .post(
+        "/companies/age",
+        {
+          ageDetails,
+          companyId: auth.getUserId(),
+        },
+        { headers: { Authorization: `Bearer ${auth.getToken()}` } }
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
 
   function updateAges(index, event) {
     const { name, value } = event.target;
-    const ages = [...ageDetails.sessions];
+    const ages = [...ageDetails];
     ages[index] = { ...ages[index], [name]: value };
-
-    setAgeDetails({ ...ageDetails, sessions: ages });
+    setAgeDetails(ages);
   }
 
   return (
     <Container className={classes.container}>
-      <Typography variant="h3">AGE GROUPS</Typography>
-      <div>
-        Add the age groups you work with here. This is important for when you
-        add your courses for the ages they apply to.
-      </div>
-      <table>
-        <tbody>
-          <tr>
-            {rows.map((el, i) => {
-              return (
-                <>
-                  <Typography variant="h6">{i + 1}: Age Group</Typography>
-                  <AgeComponent
-                    classes={classes}
-                    updateAgeDays={(e) => updateAges(i, e)}
-                    key={i}
-                  />
-                </>
-              );
-            })}
-          </tr>
-        </tbody>
-      </table>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setRows([...rows, 1])}
-      >
-        Add more
-      </Button>
-      <Button className={classes.button} variant="contained" color="primary">
-        DONE
-      </Button>
-      <Link to="/companyDashboard">
-        <Button className={classes.button} variant="outlined" color="primary">
-          Back
+      <form onSubmit={handleSubmit} className={classes.container}>
+        <Typography variant="h3">AGE GROUPS</Typography>
+        <div>
+          Add the age groups you work with here. This is important for when you
+          add your courses for the ages they apply to.
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              {rows.map((el, i) => {
+                return (
+                  <>
+                    <Typography variant="h6">{i + 1}: Age Group</Typography>
+                    <AgeComponent
+                      classes={classes}
+                      updateAges={(e) => updateAges(i, e)}
+                      key={i}
+                    />
+                  </>
+                );
+              })}
+            </tr>
+          </tbody>
+        </table>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setRows([...rows, 1])}
+        >
+          Add more
         </Button>
-      </Link>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          DONE
+        </Button>
+        <Link to="/companyDashboard">
+          <Button className={classes.button} variant="outlined" color="primary">
+            Back
+          </Button>
+        </Link>
+      </form>
     </Container>
   );
 }
