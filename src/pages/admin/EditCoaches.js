f37830a9752a8557c051c827fc8071af1776c4f8
surajkamtam <30399490+surajkamtam@users.noreply.github.com>
2020-10-23 13:25:38 +0100

    courses functions

--- src/pages/admin/EditCoaches.js
+++ src/pages/admin/EditCoaches.js
@@ -16,6 +16,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { storage } from "../../lib/firebase";
 import axios from "axios";
 import auth from "../../lib/auth";
+import BackupIcon from "@material-ui/icons/Backup";
 
 const useStyles = makeStyles((theme) => ({
   spacing: {
@@ -74,12 +75,6 @@ export default function FormPropsTextFields({ location, history }) {
     checked: false,
     details: null,
   });
-
-  const handleUpload = (e) => {
-    if (e.target.files[0]) {
-      setImage(e.target.files[0]);
-    }
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -217,11 +212,19 @@ export default function FormPropsTextFields({ location, history }) {
         </FormControl>
 
         <input
-          id="upload-photo"
-          className={classes.upload}
+          ref={input}
+          style={{ display: "none" }}
+          onChange={(e) => handleDocumentUpload(e)}
           type="file"
-          onChange={handleUpload}
         />
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => input.current.click()}
+        >
+          <BackupIcon />
+          UPLOAD COACHING CERTIFICATE
+        </Button>
 
         <Typography variant="h5"> DBS Check </Typography>
 
@@ -249,15 +252,8 @@ export default function FormPropsTextFields({ location, history }) {
           color="secondary"
           onClick={() => input.current.click()}
         >
+          <BackupIcon />
           UPLOAD DBS CERTIFICATE
-        </Button>
-
-        <Button
-          variant="contained"
-          color="default"
-          onClick={() => input.current.click()}
-        >
-          UPLOAD COACHING CERTIFICATE
         </Button>
 
         <Button
