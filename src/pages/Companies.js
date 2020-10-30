e819799beaec1c0dc17a94b7613c2408010ecda2
surajkamtam <30399490+surajkamtam@users.noreply.github.com>
2020-10-30 15:57:35 +0000

    With camp Multidetails

--- src/pages/Companies.js
+++ src/pages/Companies.js
@@ -1,32 +1,31 @@
-
-import React, { useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
-import ReactMapGL, { Marker, Popup } from 'react-map-gl'
-import { makeStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
-import LocationOnSharpIcon from '@material-ui/icons/LocationOnSharp';
-import { useEffect } from 'react';
-import axios from 'axios'
-import FilterModal from '../components/FilterModal'
-import PeopleAltSharpIcon from '@material-ui/icons/PeopleAltSharp';
-import { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
-import MoreHorizSharpIcon from '@material-ui/icons/MoreHorizSharp';
-import PersonPinCircleSharpIcon from '@material-ui/icons/PersonPinCircleSharp';
-import SupervisorAccountSharpIcon from '@material-ui/icons/SupervisorAccountSharp';
-import EventNoteSharpIcon from '@material-ui/icons/EventNoteSharp';
-import EmojiPeopleSharpIcon from '@material-ui/icons/EmojiPeopleSharp';
-import RoomSharpIcon from '@material-ui/icons/RoomSharp';
-import ReactMapPopup from '../components/ReactMapPopup'
-import { fi } from 'date-fns/locale';
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { makeStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import LocationOnSharpIcon from "@material-ui/icons/LocationOnSharp";
+import { useEffect } from "react";
+import axios from "axios";
+import FilterModal from "../components/FilterModal";
+import PeopleAltSharpIcon from "@material-ui/icons/PeopleAltSharp";
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
+import MoreHorizSharpIcon from "@material-ui/icons/MoreHorizSharp";
+import PersonPinCircleSharpIcon from "@material-ui/icons/PersonPinCircleSharp";
+import SupervisorAccountSharpIcon from "@material-ui/icons/SupervisorAccountSharp";
+import EventNoteSharpIcon from "@material-ui/icons/EventNoteSharp";
+import EmojiPeopleSharpIcon from "@material-ui/icons/EmojiPeopleSharp";
+import RoomSharpIcon from "@material-ui/icons/RoomSharp";
+import ReactMapPopup from "../components/ReactMapPopup";
+import { fi } from "date-fns/locale";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -52,10 +51,8 @@ const useStyles = makeStyles((theme) => ({
     display: "none",
   },
   header: {
-
-    minHeight: '27vh',
-    padding: '35px 35px'
-
+    minHeight: "27vh",
+    padding: "35px 35px",
   },
   divider: {
     [theme.breakpoints.up("sm")]: {
@@ -118,14 +115,14 @@ export default function Companies({ age }) {
   const [selected, setSelected] = useState();
   const [coordinates, setCoordinates] = useState({
     lat: null,
-    lng: null
-  })
-  const location = useRef()
-  const [clearFilter, setClearFilter] = useState(false)
+    lng: null,
+  });
+  const location = useRef();
+  const [clearFilter, setClearFilter] = useState(false);
   const [filterDetails, setFilterDetails] = useState({
     location: {
-      longitude: '',
-      latitude: ''
+      longitude: "",
+      latitude: "",
     },
     timing: {
       days: {
@@ -135,13 +132,13 @@ export default function Companies({ age }) {
         thursday: false,
         friday: false,
         saturday: false,
-        sunday: false
+        sunday: false,
       },
       times: {
         morning: false,
         afternoon: false,
-        evening: false
-      }
+        evening: false,
+      },
     },
     age: {
       7: false,
@@ -156,11 +153,11 @@ export default function Companies({ age }) {
       16: false,
       17: false,
       18: false,
-      adults: false
-    }
-  })
+      adults: false,
+    },
+  });
 
-  const [userCoordinates, setUserCoordinates] = useState()
+  const [userCoordinates, setUserCoordinates] = useState();
 
   const [viewport, setViewport] = useState({
     longitude: -0.13,
@@ -171,126 +168,149 @@ export default function Companies({ age }) {
   });
 
   useEffect(() => {
-    axios.get('/companies')
-      .then(res => setCompanies(res.data))
-  }, [clearFilter])
-
+    axios.get("/companies").then((res) => setCompanies(res.data));
+  }, [clearFilter]);
 
   function toggleModal(e) {
-    if (modalOpen === false) setSelectedFilter(e.target.id)
-    setModal(!modalOpen)
+    if (modalOpen === false) setSelectedFilter(e.target.id);
+    setModal(!modalOpen);
   }
 
   const handleTimingChange = (e) => {
-    const { name, checked, id } = e.target
+    const { name, checked, id } = e.target;
     setFilterDetails({
-      ...filterDetails, timing: {
+      ...filterDetails,
+      timing: {
         ...filterDetails.timing,
-        [id]: { ...filterDetails.timing[id], [name]: checked }
-      }
-    })
+        [id]: { ...filterDetails.timing[id], [name]: checked },
+      },
+    });
   };
 
   const handleAgeChange = (e) => {
-    const { name, checked } = e.target
+    const { name, checked } = e.target;
     setFilterDetails({
-      ...filterDetails, age: {
-        ...filterDetails.age, [name]: checked
-      }
-    })
+      ...filterDetails,
+      age: {
+        ...filterDetails.age,
+        [name]: checked,
+      },
+    });
   };
 
   const handleSelect = async (value) => {
-    const results = await geocodeByAddress(value)
-    const latLng = await getLatLng(results[0])
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
 
     setFilterDetails({
-      ...filterDetails, location: {
-        latitude: latLng.lat, longitude: latLng.lng
-      }
-    })
+      ...filterDetails,
+      location: {
+        latitude: latLng.lat,
+        longitude: latLng.lng,
+      },
+    });
 
-    axios.post('/filteredCompanies', {
-      ...filterDetails, location: {
-        latitude: latLng.lat, longitude: latLng.lng
-      }
-    })
-      .then(res => {
-        toggleModal()
-        setViewport({ ...viewport, zoom: 11, latitude: latLng.lat, longitude: latLng.lng })
-        setAddress(value)
-        setCoordinates(latLng)
-        setCompanies(res.data)
+    axios
+      .post("/filteredCompanies", {
+        ...filterDetails,
+        location: {
+          latitude: latLng.lat,
+          longitude: latLng.lng,
+        },
       })
-  }
+      .then((res) => {
+        toggleModal();
+        setViewport({
+          ...viewport,
+          zoom: 11,
+          latitude: latLng.lat,
+          longitude: latLng.lng,
+        });
+        setAddress(value);
+        setCoordinates(latLng);
+        setCompanies(res.data);
+      });
+  };
 
   const success = async (pos) => {
-    location.current.innerHTML = 'Successfully located!'
-    var crd = pos.coords
-    const position = { latitude: parseFloat(crd.latitude.toFixed(4)), longitude: parseFloat(crd.longitude.toFixed(4)) }
+    location.current.innerHTML = "Successfully located!";
+    var crd = pos.coords;
+    const position = {
+      latitude: parseFloat(crd.latitude.toFixed(4)),
+      longitude: parseFloat(crd.longitude.toFixed(4)),
+    };
 
-    setFilterDetails({ ...filterDetails, location: position })
+    setFilterDetails({ ...filterDetails, location: position });
 
-
-    axios.post('/filteredCompanies', { ...filterDetails, location: position })
-      .then(res => {
-
-        setUserCoordinates(position)
+    axios
+      .post("/filteredCompanies", { ...filterDetails, location: position })
+      .then((res) => {
+        setUserCoordinates(position);
         setViewport({
           ...viewport,
           zoom: 11,
           latitude: parseFloat(position.latitude),
-          longitude: parseFloat(position.longitude)
-        })
-        setCompanies(res.data)
-      })
-
-  }
+          longitude: parseFloat(position.longitude),
+        });
+        setCompanies(res.data);
+      });
+  };
 
   const handleLocate = (e) => {
-    location.current.innerHTML = 'Locating...'
+    location.current.innerHTML = "Locating...";
     setTimeout(() => {
-      navigator.geolocation.getCurrentPosition(success)
-    }, 1500)
-  }
-
-
+      navigator.geolocation.getCurrentPosition(success);
+    }, 1500);
+  };
 
   function handleFilterSubmit() {
+    console.log(filterDetails);
 
-    console.log(filterDetails)
-
-    axios.post('/filteredCompanies', filterDetails)
-      .then(res => {
-        toggleModal()
-        setCompanies(res.data)
-      })
+    axios.post("/filteredCompanies", filterDetails).then((res) => {
+      toggleModal();
+      setCompanies(res.data);
+    });
   }
 
   const filterIcons = {
     Location: LocationOnSharpIcon,
     Age: SupervisorAccountSharpIcon,
-    Timing: EventNoteSharpIcon
-  }
+    Timing: EventNoteSharpIcon,
+  };
 
   const handleClearFilters = () => {
-    setClearFilter(!clearFilter)
+    setClearFilter(!clearFilter);
     setFilterDetails({
-      location: { longitude: '', latitude: '' },
+      location: { longitude: "", latitude: "" },
       timing: {
         days: {
-          monday: false, tuesday: false, wednesday: false, thursday: false,
-          friday: false, saturday: false, sunday: false
+          monday: false,
+          tuesday: false,
+          wednesday: false,
+          thursday: false,
+          friday: false,
+          saturday: false,
+          sunday: false,
         },
-        times: { morning: false, afternoon: false, evening: false }
+        times: { morning: false, afternoon: false, evening: false },
       },
       age: {
-        7: false, 8: false, 9: false, 10: false, 11: false, 12: false, 13: false,
-        14: false, 15: false, 16: false, 17: false, 18: false, adults: false
-      }
-    })
-  }
-
+        7: false,
+        8: false,
+        9: false,
+        10: false,
+        11: false,
+        12: false,
+        13: false,
+        14: false,
+        15: false,
+        16: false,
+        17: false,
+        18: false,
+        adults: false,
+      },
+    });
+  };
 
   return (
     <>
@@ -310,8 +330,8 @@ export default function Companies({ age }) {
                 const Icon = filterIcons[el];
                 let text;
 
-                if (el === 'Timing') text = 'Date/Times'
-                else text = el
+                if (el === "Timing") text = "Date/Times";
+                else text = el;
                 return (
                   <Button
                     id={el}
@@ -327,7 +347,6 @@ export default function Companies({ age }) {
                 );
               })}
             </div>
-
           </header>
 
           <Divider className={classes.divider} variant="middle" />
@@ -354,120 +373,125 @@ export default function Companies({ age }) {
 
           {/* map through companies */}
 
+          {companies ? (
+            companies.map((el, i) => {
+              const { name, images, bio } = el.companyInfo;
+              return (
+                <>
+                  <Link
+                    key={i}
+                    to={{
+                      pathname: `/companies/${el.companyId}`,
+                      state: el.companyInfo,
+                    }}
+                  >
+                    <Card className={classes.card}>
+                      <CardActionArea className={classes.cardSubcontainer}>
+                        <CardMedia
+                          className={classes.media}
+                          image={
+                            images
+                              ? images[0]
+                              : "https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                          }
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            {name}
+                          </Typography>
+                          <Typography
+                            // className='company-bio-preview'
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                          >
+                            {/* {bio} */}
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Donec congue faucibus felis, vel semper tellus
+                            eleifend eu. Vestibulum ullamcorper ultrices
+                            efficitur.
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
 
-          {companies ? companies.map((el, i) => {
-            const { name, images, bio } = el.companyInfo
-            return (
-              <>
-
-                <Link key={i} to={{
-                  pathname: `/companies/${el.companyId}`,
-                  state: el.companyInfo
-
-                }}>
-
-                  <Card className={classes.card}>
-                    <CardActionArea className={classes.cardSubcontainer}>
-                      <CardMedia
-                        className={classes.media}
-                        image={images ? images[0] :
-                          "https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {name}
-                        </Typography>
-                        <Typography 
-                        // className='company-bio-preview' 
-                        variant="body2" color="textSecondary" component="p">
-                          {/* {bio} */}
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue faucibus felis,
-                          vel semper tellus eleifend eu. Vestibulum ullamcorper ultrices efficitur.
-                    </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-
-                  <Divider className={classes.divider} variant='middle' />
-
-                </Link>
-
-              </>
-            )
-          }) : <CircularProgress className={classes.progress} color="secondary" />}
-
+                    <Divider className={classes.divider} variant="middle" />
+                  </Link>
+                </>
+              );
+            })
+          ) : (
+            <CircularProgress className={classes.progress} color="secondary" />
+          )}
         </section>
         <section className={classes.mapContainer}>
           <ReactMapGL
             {...viewport}
             mapboxApiAccessToken={key}
-
-            mapStyle='mapbox://styles/seangpachareonsub/ckdsqcwif16xt19mlhiuwb2dt'
-            onViewportChange={viewport => {
-              setViewport(viewport)
-            }}>
-
-            {companies && companies.map(el => {
-
-              if (el.companyInfo.location) {
-                const { latitude, longitude } = el.companyInfo.location
-                return (
-                  <Marker key={el.companyId}
-                    anchor={'top-left'}
-                    offsetLeft={-20}
-                    offsetTop={-30}
-                    latitude={latitude}
-                    longitude={longitude} >
-                    <RoomSharpIcon
-                      onClick={() => {
-                        if (selected) {
-                          if (selected === el) {
-                            setSelected(null)
-                          } else setSelected(el)
-                        } else setSelected(el)
-                      }}
-                      style={{
-                        fontSize: '40px',
-                        color: 'red'
-                      }} />
-                  </Marker>
-                )
-              }
-            }
-            )}
+            mapStyle="mapbox://styles/seangpachareonsub/ckdsqcwif16xt19mlhiuwb2dt"
+            onViewportChange={(viewport) => {
+              setViewport(viewport);
+            }}
+          >
+            {companies &&
+              companies.map((el) => {
+                if (el.companyInfo.location) {
+                  const { latitude, longitude } = el.companyInfo.location;
+                  return (
+                    <Marker
+                      key={el.companyId}
+                      anchor={"top-left"}
+                      offsetLeft={-20}
+                      offsetTop={-30}
+                      latitude={latitude}
+                      longitude={longitude}
+                    >
+                      <RoomSharpIcon
+                        onClick={() => {
+                          if (selected) {
+                            if (selected === el) {
+                              setSelected(null);
+                            } else setSelected(el);
+                          } else setSelected(el);
+                        }}
+                        style={{
+                          fontSize: "40px",
+                          color: "red",
+                        }}
+                      />
+                    </Marker>
+                  );
+                }
+              })}
 
             {userCoordinates && (
               <Marker
-                anchor={'top-left'}
+                anchor={"top-left"}
                 offsetLeft={-20}
                 offsetTop={-30}
                 latitude={parseFloat(userCoordinates.latitude)}
-                longitude={parseFloat(userCoordinates.longitude)} >
+                longitude={parseFloat(userCoordinates.longitude)}
+              >
                 <PersonPinCircleSharpIcon
                   style={{
-                    fontSize: '40px',
-                    color: 'blue'
-                  }} />
+                    fontSize: "40px",
+                    color: "blue",
+                  }}
+                />
               </Marker>
             )}
 
-            {selected && <ReactMapPopup
-              selected={selected} />}
-
-
-
-
+            {selected && <ReactMapPopup selected={selected} />}
           </ReactMapGL>
         </section>
       </div>
 
-      {
-        modalOpen && <FilterModal
+      {modalOpen && (
+        <FilterModal
           handleFilterSubmit={(e) => handleFilterSubmit(e)}
           filterDetails={filterDetails}
           handleTimingChange={(e) => handleTimingChange(e)}
           handleAgeChange={(e) => handleAgeChange(e)}
-
           selectedFilter={selectedFilter}
           classes={classes}
           address={address}
