8366707d411be4c4b34037e0639b81664dcca1d9
Kenn <kennkenns@hotmail.com>
2021-02-08 16:47:53 +0000

    monday

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ import SuccessfulCheckout from './pages/SuccessfulCheckout'
 const stripePromise = loadStripe('pk_test_JX84GPfLfXGxVFWvGHaz1AWE')
 
 
-// axios.defaults.baseURL = "https://europe-west2-football-hub-4018a.cloudfunctions.net/api"
+axios.defaults.baseURL = "https://europe-west2-football-hub-4018a.cloudfunctions.net/api"
 
 const App = () => {
   const [userType, setUserType] = useState()
