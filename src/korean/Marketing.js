9be6c4a2a9b70065e991f03362db8a8e8695ffef
Kenn <kennkenns@hotmail.com>
2021-02-11 23:54:48 +0000

    thursday night

--- src/korean/Marketing.js
+++ src/korean/Marketing.js
@@ -5,117 +5,263 @@ import {
   Grid,
   Fab,
   Box,
-  Switch
+  Switch,
+  Card,
+  CardActions,
+  CardContent,
+  Button
 } from "@material-ui/core";
-// import Switch from '@material-ui/core/Switch';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
+import HomeNav from '../components/Navbars/HomeNav'
+import StarsSharpIcon from '@material-ui/icons/StarsSharp';
+import EventSharpIcon from '@material-ui/icons/EventSharp';
+import ScheduleSharpIcon from '@material-ui/icons/ScheduleSharp';
+import RoomSharpIcon from '@material-ui/icons/RoomSharp';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 const useStyles = makeStyles((theme) => ({
-  text: {
-    fontSize: '12px'
-  },
-  fab: {
-    height: '40px',
-    position: 'absolute',
-    right: '2%',
-    bottom: '2%'
-  },
   root: {
     width: '100vw',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
   },
-  section: {
+  main: {
+    width: '100%',
     height: '100%',
-    width: '80%',
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: '120px',
+    [theme.breakpoints.up('md')]: {
+      width: '80%',
+    },
+  },
+  jumbotron: {
+    minHeight: '55vh',
+    margin: '55px 0',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     justifyContent: 'space-between',
     [theme.breakpoints.up('md')]: {
       flexDirection: 'row'
     },
   },
+  media: {
+    width: '80%',
+    [theme.breakpoints.up('md')]: {
+      width: '49%',
+    },
+  },
+  jumbotronText: {
+    width: '80%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    height: '55vh',
+    [theme.breakpoints.up('md')]: {
+      width: '45%',
+    },
+  },
+  list: {
+    margin: '13px 0',
+    listStyleType: 'circle'
+  },
+  subsection: {
+    width: '100%',
+    minHeight: '70vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginBottom: 40,
+    [theme.breakpoints.up('md')]: {
+      width: '80%',
+    },
+  },
+  cardContainer: {
+    display: 'flex',
+    flex: 1,
+    flexWrap: 'wrap',
+    height: '85%',
+    justifyContent: 'space-around',
+    alignContent: 'space-evenly'
+  },
+  card: {
+    minWidth: 220,
+    width: '23%'
+  },
+  campCardContainer: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  campCard: {
+    height: '35vh',
+    width: '30%',
+    position: 'relative'
+  },
+  campCardTextContainer: {
+    height: '80%'
+  },
+  campCardText: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '20px 30px'
+  },
+  button: {
+    position: 'absolute',
+    bottom: '4%',
+    right: '3%'
+
+  }
 }));
 
-const Marketing = () => {
+const Marketing = (props) => {
   const classes = useStyles()
-  const [locale, setLocale] = useState('ko')
-  const AntSwitch = withStyles((theme) => ({
-    root: {
-      width: 28,
-      height: 16,
-      padding: 0,
-      display: 'flex',
-    },
-    switchBase: {
-      padding: 2,
-      color: theme.palette.grey[500],
-      '&$checked': {
-        transform: 'translateX(12px)',
-        color: theme.palette.common.white,
-        '& + $track': {
-          opacity: 1,
-          backgroundColor: theme.palette.primary.main,
-          borderColor: theme.palette.primary.main,
-        },
-      },
-    },
-    thumb: {
-      width: 12,
-      height: 12,
-      boxShadow: 'none',
-    },
-    track: {
-      opacity: 1,
-      borderRadius: 16 / 2,
-      backgroundColor: theme.palette.secondary.main,
-      borderColor: theme.palette.secondary.main,
-    },
-    checked: {},
-  }))(Switch);
+  const { locale, history } = props
 
+  const {
+    mainTitle,
+    headerMain,
+    headerParagraph,
+    headerList,
+    subSectionHeaders,
+    subSectionCards,
+    subSectionCampsTitle,
+    subSectionCampsCards,
+    subSectionCampsCardButton
+  } = languages[locale].marketingPage
 
-  const fab = (
-    <Fab className={classes.fab} variant="extended">
-      <Typography component="div">
-        <Grid component="label" container alignItems="center" spacing={1}>
-          <Grid className={classes.text} item> KR </Grid>
-          <Grid item>
-            <AntSwitch
-              checked={locale === 'en'}
-              onChange={e => e.target.checked ? setLocale('en') : setLocale('ko')}
-              name="checkedC" />
-          </Grid>
-          <Grid className={classes.text} item> EN </Grid>
-        </Grid>
-      </Typography>
-    </Fab>
-  )
 
 
   return (
     <div className={classes.root}>
-      {fab}
+      <main className={classes.main}>
+        <Typography component='div' >
+          <Box
+            fontSize={35} fontWeight="fontWeightBold" m={0}>
+            {mainTitle}
+          </Box>
+        </Typography>
 
+        <div className={classes.jumbotron}>
+          <img className={classes.media}
+            src="https://images.unsplash.com/photo-1529153348965-3de49b6188bd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" alt="" />
 
-      <main>
-       
-        <h1>
-          {languages[locale].mainTitle}
-        </h1>
+          <section className={classes.jumbotronText}>
+            <Typography component='div' >
+              <Box
+                fontSize={28} fontWeight="fontWeightRegular" m={0}>
+                {headerMain}
+              </Box>
+            </Typography>
 
-        <div>
-          <h2>
-            {languages[locale].headerMain}
-          </h2>
+            <p> {headerParagraph} </p>
 
-          <p>
-          {languages[locale].headerParagraph}
-          </p>
+            <ul>
+              <li className={classes.list}> {headerList} </li>
+              <li className={classes.list}> {headerList} </li>
+              <li className={classes.list}> {headerList} </li>
+              <li className={classes.list}> {headerList} </li>
+            </ul>
+          </section>
+        </div>
+      </main>
+
+      <section className={classes.subsection}>
+        <Typography component='div'>
+          <Box
+            style={{ color: 'orange' }}
+            fontSize={14} fontWeight="fontWeightBold" m={0}>
+            {subSectionHeaders[0]}
+          </Box>
+          <Box
+            style={{ margin: '10px 0 15px 0' }}
+            fontSize={30} fontWeight="fontWeightBold" m={0}>
+            {subSectionHeaders[1]}
+          </Box>
+          <Box
+            fontSize={16} fontWeight="fontWeightRegular" m={0}>
+            {subSectionHeaders[2]}
+          </Box>
+        </Typography>
+
+        <div className={classes.cardContainer}>
+          {subSectionCards.map(el => {
+            return (
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {el.title}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {el.paragraph}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <StarsSharpIcon />
+                </CardActions>
+              </Card>
+            )
+          })}
         </div>
 
-      </main>
+      </section>
+
+      <section className={classes.subsection}>
+        <Typography style={{ marginBottom: '25px' }} component='div'>
+          <Box
+            fontSize={30} fontWeight="fontWeightBold" m={0}>
+            {subSectionCampsTitle}
+          </Box>
+        </Typography>
+
+        <div className={classes.campCardContainer}>
+          {subSectionCampsCards.map(el => {
+            return (
+              <Card variant="outlined" className={classes.campCard}>
+                <CardContent className={classes.campCardTextContainer}>
+                  <Typography variant="h6" component="h2">
+                    {el.age}
+                  </Typography>
+                  <Typography
+                    className={classes.campCardText}
+                    variant="body2" component="p">
+                    <EventSharpIcon
+                      style={{ marginRight: '15px' }} /> {el.date}
+                  </Typography>
+                  <Typography
+                    className={classes.campCardText}
+                    variant="body2" component="p">
+                    <ScheduleSharpIcon
+                      style={{ marginRight: '15px' }} /> {el.days}
+                  </Typography>
+                  <Typography
+                    className={classes.campCardText}
+                    variant="body2" component="p">
+                    <RoomSharpIcon
+                      style={{ marginRight: '15px' }} /> {el.location}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    className={classes.button}
+                    onClick={() => history.push('/apply')}
+                    endIcon={<ArrowForwardIcon />}>
+                    {subSectionCampsCardButton}
+                  </Button>
+                </CardActions>
+              </Card>
+            )
+          })}
+        </div>
+      </section>
 
     </div>
   );
---
+++ src/korean/SwitchLanguageFab.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom'
+import {
+  Typography,
+  Grid,
+  Fab,
+  Switch,
+} from "@material-ui/core";
+import { withStyles, makeStyles} from '@material-ui/core/styles';
+
+
+
+const SwitchLanguageFab = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    text: {
+      fontSize: '12px'
+    },
+    fab: {
+      height: '40px',
+      position: 'fixed',
+      right: '2%',
+      bottom: '2%'
+    },
+  }))
+  const classes = useStyles()
+  const AntSwitch = withStyles((theme) => ({
+    root: {
+      width: 28,
+      height: 16,
+      padding: 0,
+      display: 'flex',
+    },
+    switchBase: {
+      padding: 2,
+      color: theme.palette.grey[500],
+      '&$checked': {
+        transform: 'translateX(12px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          opacity: 1,
+          backgroundColor: theme.palette.primary.main,
+          borderColor: theme.palette.primary.main,
+        },
+      },
+    },
+    thumb: {
+      width: 12,
+      height: 12,
+      boxShadow: 'none',
+    },
+    track: {
+      opacity: 1,
+      borderRadius: 16 / 2,
+      backgroundColor: theme.palette.secondary.main,
+      borderColor: theme.palette.secondary.main,
+    },
+    checked: {},
+  }))(Switch);
+
+  const { handleLocaleChange, locale } = props
+
+  // console.log(props)
+  // console.log(locale, handleLocaleChange)
+
+  return (
+    <Fab className={classes.fab} variant="extended">
+      <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid className={classes.text} item> KR </Grid>
+          <Grid item>
+            <AntSwitch
+              checked={locale === 'en'}
+              onChange={(e) => handleLocaleChange(e)}
+              name="checkedC" />
+          </Grid>
+          <Grid className={classes.text} item> EN </Grid>
+        </Grid>
+      </Typography>
+    </Fab>
+  );
+};
+
+export default withRouter(SwitchLanguageFab);
+
