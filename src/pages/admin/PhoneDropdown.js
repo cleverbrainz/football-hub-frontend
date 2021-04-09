b69bef88a32f54f0e092533b707e8f0755342589
patrickcfwhite <patrick.cf.white@gmail.com>
2021-04-09 09:54:48 +0100

    2fa style updates

--- src/pages/admin/PhoneDropdown.js
+++ src/pages/admin/PhoneDropdown.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { option, select } from '@material-ui/core'
 
 
-const PhoneDropDown = ({}) => {
+const PhoneDropDown = ({locale}) => {
   
   
   
@@ -11,8 +11,8 @@ const PhoneDropDown = ({}) => {
   
   return (
       <>
-        <option data-countryCode="KR" value="82">Korea, South (+82)</option>
-        <option data-countryCode="GB" value="44">United Kingdom (+44)</option>
+        <option data-countryCode="KR" value="82">{locale === 'ko' ? '대한민국' : 'Korea, South'} (+82)</option>
+        <option data-countryCode="GB" value="44">UK (+44)</option>
       </>
   )
 }
